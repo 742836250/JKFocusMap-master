1906ad5e66d10194846b7f4417841bc75c44b21b
wangruifeng <wangruifeng@xiangrikui.com>
2016-01-03 14:13:42 +0800

    Initial Commit

---
+++ JKFocusMap-master/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JKFocusMap-master
+//
+//  Created by 王锐锋 on 16/1/3.
+//  Copyright © 2016年 jack_wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
